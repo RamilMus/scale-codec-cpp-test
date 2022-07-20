47544d4070179e8ddec9b8845cf5b57afda50b54
RamilMus <94644460+RamilMus@users.noreply.github.com>
2022-07-20 23:14:50 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,6 @@
 BUILD_DIR="${BUILD_DIR:?BUILD_DIR variable is not defined}"
 BUILD_TARGET="${BUILD_TARGET:-test}"
 
-JOBS_NUM="$(( $(nproc 2>/dev/null || sysctl -n hw.ncpu) + 1 ))"
-MAKE_ARGS="${MAKE_ARGS:-"-j$JOBS_NUM"}"
-
 cd "$(dirname "$0")/.."
 
 git submodule update --init
