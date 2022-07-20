355a58daa3df6258c4723b442492f307727a3566
RamilMus <94644460+RamilMus@users.noreply.github.com>
2022-07-21 02:53:21 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,9 @@
 BUILD_DIR="${BUILD_DIR:?BUILD_DIR variable is not defined}"
 BUILD_TARGET="${BUILD_TARGET:-test}"
 
+JOBS_NUM="$(( $(nproc 2>/dev/null || sysctl -n hw.ncpu) + 1 ))"
+MAKE_ARGS="${MAKE_ARGS:-"-j$JOBS_NUM"}"
+
 cd "$(dirname "$0")/.."
 
 git submodule update --init
@@ -21,4 +24,3 @@ if [ "${BUILD_TARGET}" == "test" ]; then
   cmake --build "${BUILD_DIR}" -- "${MAKE_ARGS}"
 fi
 cmake --build "${BUILD_DIR}" --target "${BUILD_TARGET}" -- "${MAKE_ARGS}"
-
