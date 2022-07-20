222dc6e949a8d9c290d00a6d12df0ec90f229d3f
RamilMus <94644460+RamilMus@users.noreply.github.com>
2022-07-21 01:04:30 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,13 +5,19 @@ ls -la
 echo "test1111111"
 
 mkdir build && cd build
-
+pwd
+echo "hhhhhh"
 git submodule update --init
 
 cmake ..
+pwd
+echo "hhhhhh"
 cmake --build .
 
 echo "estsetsetsetset"
+cd ..
+pwd
+echo "hhhhhh"
 ctest -C Debug
 echo "donedonedone"
 
