baeb4ca10617a8b47f07cf2b165dab8ab6de1b10
RamilMus <94644460+RamilMus@users.noreply.github.com>
2022-07-21 01:45:18 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash -xe
 
-CMAKE_ARGS+=( "-DBUILD_TESTS=ON" )
+BUILD_DIR="${BUILD_DIR:?BUILD_DIR variable is not defined}"
+BUILD_TARGET="${BUILD_TARGET:-test}"
 
-mkdir build && cd build
-pwd
+cd "$(dirname "$0")/.."
+
 git submodule update --init
 
-cmake ..
-cmake --build .
+CMAKE_ARGS=( "-B" "${BUILD_DIR}" "$@" )
 
-echo "estsetsetsetset"
-ctest -C Debug
-echo "donedonedone"
+# see clang-tidy build in ci.yml github workflow
+if [ "${BUILD_TARGET}" == "test" ]; then
+  CMAKE_ARGS+=( "-DBUILD_TESTS=ON" )
+fi
+
+cmake . "${CMAKE_ARGS[@]}"
+
+if [ "${BUILD_TARGET}" == "test" ]; then
+  # first build test executables
+  cmake --build "${BUILD_DIR}" -- "${MAKE_ARGS}"
+fi
+cmake --build "${BUILD_DIR}" --target "${BUILD_TARGET}" -- "${MAKE_ARGS}"
 
