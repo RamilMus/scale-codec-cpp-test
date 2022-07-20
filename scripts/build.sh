2a29363bf9ffd98ea642d9c2e4e4e4d51520b352
RamilMus <94644460+RamilMus@users.noreply.github.com>
2022-07-21 01:29:44 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,15 @@
 #!/bin/bash -xe
 
-pwd
-ls -la
-echo "test1111111"
+CMAKE_ARGS+=( "-DBUILD_TESTS=ON" )
 
 mkdir build && cd build
 pwd
-echo "hhhhhh"
 git submodule update --init
 
 cmake ..
-pwd
-echo "hhhhhh"
 cmake --build .
 
 echo "estsetsetsetset"
-cd ..
-pwd
-echo "hhhhhh"
 ctest -C Debug
 echo "donedonedone"
 
