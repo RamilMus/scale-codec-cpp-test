17a3a45b8990b95ebbece11cd15970685def4bfa
RamilMus <94644460+RamilMus@users.noreply.github.com>
2022-07-21 00:56:41 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -xe
 
+pwd
+ls -la
+echo "test1111111"
+
 mkdir build && cd build
+
+git submodule update --init
+
 cmake ..
 cmake --build .
+
+echo "estsetsetsetset"
 ctest -C Debug
+echo "donedonedone"
 
